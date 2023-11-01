79529bcbd39c69fcf3a625941e99fa65e785a78c
abdillah <abdillahnoersaid@gmail.com>
2023-11-01 09:57:47 +0700

    Sistem Akademik

--- KRS6.java
+++ KRS6.java
@@ -1,18 +1,22 @@
-import java.util.*;
 public class KRS6 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        String[] matkul = {"Pancasila", "KTI", "CTPS", "Matdas", "B ing", "Daspro", "Praktikum Daspro", "K3"};
-        int[] semester = {1, 1, 1, 1, 1, 1, 1, 1};
-        int[] sks = {2, 2, 2, 3, 2, 2, 3, 2};
-        int[] jam = {2, 4, 4, 6, 4, 4, 6, 4};
+        String[][] matkulData = {
+            {"Pancasila", "1", "2", "2"},
+            {"KTI", "1", "2", "4"},
+            {"CTPS", "1", "2", "4"},
+            {"Matdas", "1", "3", "6"},
+            {"B ing", "1", "2", "4"},
+            {"Daspro", "1", "2", "4"},
+            {"Praktikum Daspro", "1", "3", "6"},
+            {"K3", "1", "2", "4"}
+        };
 
         System.out.println("-----------------------------------------------");
         System.out.println("| Matkul          |   Semester  |  SKS  | Jam |");
         System.out.println("-----------------------------------------------");
-        for (int i = 0; i < matkul.length; i++) {
-            System.out.printf("%-18s%-11s%-7s%-4s\n", "| " + matkul[i], "|      " + semester[i],"   |   " + sks[i], "   |  " + jam[i] + "  |");
+        for (String[] data : matkulData) {
+            System.out.printf("%-18s%-11s%-7s%-4s\n", "| " + data[0], "|      " + data[1], "   |   " + data[2], "   |  " + data[3] + "  |");
             System.out.println("-----------------------------------------------");
         }
     }
